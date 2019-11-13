9c76aebff1f9c8649df711051d2bc62314a86f92
zhanghao05 <cmcesummer@126.com>
2019-11-13 15:03:13 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import {render} from 'react-dom';
-import App from "./app";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-render(App, document.querySelector("#root"));
+ReactDOM.render(<App />, document.getElementById("root"));
